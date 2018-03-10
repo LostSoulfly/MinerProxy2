8970543cae1a560ec2abe530561606714f0c92ed
LostSoulFly <lostsoulfly@users.noreply.github.com>
2018-03-10 18:45:51 -0500

    Implement Eth PoolHandler

--- MinerProxy2/Coins/Ethereum/EthereumPoolHandler.cs
+++ MinerProxy2/Coins/Ethereum/EthereumPoolHandler.cs
@@ -2,60 +2,79 @@
 using MinerProxy2.Network;
 using MinerProxy2.Network.Sockets;
 using MinerProxy2.Pools;
+using Serilog;
 using System;
+using System.Text;
 
 namespace MinerProxy2.Coins
 {
     internal class EthereumPoolHandler : ICoinHandlerPool
     {
+        private MinerServer _minerServer;
+        private PoolClient _pool;
+        private PoolManager _poolManager;
+        private byte[] currentWork;
+
         public void BroadcastToMiners(byte[] data)
         {
-            throw new NotImplementedException();
+            //This will probably be removed, I don't think it's necessary..
+            _minerServer.BroadcastToMiners(data);
         }
 
         public void PoolConnected(PoolClient poolClient)
         {
-            throw new NotImplementedException();
+            Log.Information("Pool connected: " + poolClient.poolEndPoint);
         }
 
         public void PoolDataReceived(byte[] data, PoolClient poolClient)
         {
-            throw new NotImplementedException();
+            //process pool data here
+            //such as pushing work to all miners
+            //or letting miners know the share they submitted was accepted/rejected.
+            Log.Information("Pool " + poolClient.poolEndPoint + " sent: " + Encoding.ASCII.GetString(data));
+
+            //This is a simple recording of last data incase a new miner connects after the pool has connected
+            //but the idea is to keep the current work to send to new miners going forward.
+            currentWork = data;
+
+            //This is just for testing with 1 miner.
+            _minerServer.BroadcastToMiners(data);
+
         }
 
         public void PoolDisconnected(PoolClient poolClient)
         {
-            throw new NotImplementedException();
+            Log.Information("Pool disconnected: " + poolClient.poolEndPoint);
         }
 
         public void PoolError(Exception exception, PoolClient poolClient)
         {
-            throw new NotImplementedException();
+            Log.Error(exception, "Pool Error");
         }
 
         public void SendToMiner(byte[] data, TcpConnection connection)
         {
-            throw new NotImplementedException();
+            _minerServer.SendToMiner(data, connection);
         }
 
         public void SendToPool(byte[] data)
         {
-            throw new NotImplementedException();
+            _pool.SendToPool(data);
         }
 
         public void SetMinerServer(MinerServer minerServer)
         {
-            throw new NotImplementedException();
+            _minerServer = minerServer;
         }
 
         public void SetPool(PoolClient pool)
         {
-            throw new NotImplementedException();
+            _pool = pool;
         }
 
         public void SetPoolManager(PoolManager poolManager)
         {
-            throw new NotImplementedException();
+            _poolManager = poolManager;
         }
     }
 }
