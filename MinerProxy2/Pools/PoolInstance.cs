25a67ae771dfc010442683c1f40e45569b7d6825
LostSoulFly <lostsoulfly@users.noreply.github.com>
2018-03-10 22:34:23 -0500

    Change PoolItem/Instance

--- MinerProxy2/Pools/PoolInstance.cs
+++ MinerProxy2/Pools/PoolInstance.cs
@@ -7,25 +7,25 @@ namespace MinerProxy2.Pools
         public List<PoolItem> failoverPools = new List<PoolItem>();
         public readonly PoolItem mainPool;
         private PoolItem currentPool;
+        public int localListenPort;
         public bool passwordAsWorkerName;
         public bool useDotBeforeWorkerName;
         //failure attempts, then switch
         //retry main pool in seconds
 
-        private PoolInstance(string host, int port, string coin, int maxMiners)
+        public PoolInstance(string host, int port, string coin)
         {
             mainPool.poolAddress = host;
             mainPool.poolPort = port;
             mainPool.coin = coin;
-            mainPool.maxMiners = maxMiners;
         }
 
-        private void AddFailoverPool(string host, int port)
+        public void AddFailoverPool(string host, int port)
         {
-            failoverPools.Add(new PoolItem(host, port, mainPool.coin, mainPool.poolWorkerName, mainPool.maxMiners, mainPool.donationPercent));
+            failoverPools.Add(new PoolItem(host, port, mainPool.coin, mainPool.poolWorkerName, mainPool.donationPercent));
         }
 
-        private PoolItem GetCurrentPool()
+        public PoolItem GetCurrentPool()
         {
             if (currentPool == null)
                 currentPool = mainPool;
@@ -33,7 +33,7 @@ namespace MinerProxy2.Pools
             return this.currentPool;
         }
 
-        private PoolItem GetFailoverPool()
+        public PoolItem GetFailoverPool()
         {
             //check which pool we're on, then get the next failover or return mainpool
 
--- MinerProxy2/Pools/PoolItem.cs
+++ MinerProxy2/Pools/PoolItem.cs
@@ -5,16 +5,16 @@
         public string poolAddress { get; set; }
         public int poolPort { get; set; }
         public string coin { get; set; }
-        public int maxMiners { get; set; }
+        //public int maxMiners { get; set; }
         public int donationPercent { get; set; }
         public string poolWorkerName { get; set; }
 
-        public PoolItem(string host, int port, string poolWorkerName, string coin, int maxMiners, int donation)
+        public PoolItem(string host, int port, string poolWorkerName, string coin, int donation)
         {
             this.poolAddress = host;
             this.poolPort = port;
             this.coin = coin;
-            this.maxMiners = maxMiners;
+            //this.maxMiners = maxMiners;
             this.donationPercent = donation;
             this.poolWorkerName = poolWorkerName;
         }
