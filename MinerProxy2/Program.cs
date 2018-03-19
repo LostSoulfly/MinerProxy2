d956284bcb5cd9e54fe583cf8a571806b466485a
LostSoulFly <lostsoulfly@users.noreply.github.com>
2018-03-18 21:04:46 -0400

    Two proxies for the price of one

--- MinerProxy2/Program.cs
+++ MinerProxy2/Program.cs
@@ -32,12 +32,20 @@ namespace MinerProxy2
             ICoinHandlerMiner coinHandler = (ICoinHandlerMiner)new EthereumMinerHandler();
             ICoinHandlerPool poolHandler = (ICoinHandlerPool)new EthereumPoolHandler();
 
-            string server = "us1.ethermine.org"; //"ubq.pool.sexy";
-            int port = 4444; //9009;
+            string server = "us1.ethermine.org";
+            int port = 4444;
             
-
             PoolInstance ethPoolInstance = new PoolInstance(server, port, 9000, "MProxy", "0x3Ff3CF71689C7f2f8F5c1b7Fc41e030009ff7332", "ETH");
             PoolClient ethereumPool = new PoolClient(ethPoolInstance, poolHandler, coinHandler);
+
+            // ---- second proxy
+
+            ICoinHandlerMiner coinHandler2 = (ICoinHandlerMiner)new EthereumMinerHandler();
+            ICoinHandlerPool poolHandler2 = (ICoinHandlerPool)new EthereumPoolHandler();
+            string server2 = "us1-etc.ethermine.org";
+            int port2 = 4444;
+            PoolInstance ethPoolInstance2 = new PoolInstance(server2, port2, 9001, "MProxy", "0x83D557A1E88C9E3BbAe51DFA7Bd12CF523B28b84", "ETH");
+            PoolClient ethereumPool2 = new PoolClient(ethPoolInstance2, poolHandler2, coinHandler2);
 
             Console.ReadLine();
         }
