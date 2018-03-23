98dc443beae4a9fd0aafe8824938ad9329a5cea8
LostSoulFly <lostsoulfly@users.noreply.github.com>
2018-03-22 20:50:12 -0400

    Verbose to log file

--- MinerProxy2/Program.cs
+++ MinerProxy2/Program.cs
@@ -27,11 +27,12 @@ namespace MinerProxy2
                 .MinimumLevel.ControlledBy(logLevel)
                 .WriteTo.Console(theme: SystemConsoleTheme.Literate, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug)
                 //.WriteTo.File(path: AppDomain.CurrentDomain.BaseDirectory + "log.txt")
-                .WriteTo.File(path: AppDomain.CurrentDomain.BaseDirectory + "error.txt", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error)
+                .WriteTo.File(path: AppDomain.CurrentDomain.BaseDirectory + "verbose.txt", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose)
                 .CreateLogger();
 
             Helpers.Logging.MinerProxyHeader();
 
+            /*
             ICoinHandlerMiner coinHandler = (ICoinHandlerMiner)new EthereumMinerHandler();
             ICoinHandlerPool poolHandler = (ICoinHandlerPool)new EthereumPoolHandler();
 
@@ -40,17 +41,18 @@ namespace MinerProxy2
 
             PoolInstance ethPoolInstance = new PoolInstance(server, port, 9000, "MProxy", "MPbQqqbkn9en1AL3UMJNUtCBz9fPz4SGU6", "ETH");
             PoolClient ethereumPool = new PoolClient(ethPoolInstance, poolHandler, coinHandler);
+            */
 
             // ---- second proxy
 
-            /*
+            
             ICoinHandlerMiner coinHandler2 = (ICoinHandlerMiner)new EthereumMinerHandler();
             ICoinHandlerPool poolHandler2 = (ICoinHandlerPool)new EthereumPoolHandler();
             string server2 = "us1-etc.ethermine.org";
             int port2 = 4444;
-            PoolInstance ethPoolInstance2 = new PoolInstance(server2, port2, 9001, "DevFee", "0x83D557A1E88C9E3BbAe51DFA7Bd12CF523B28b84", "ETH");
+            PoolInstance ethPoolInstance2 = new PoolInstance(server2, port2, 9000, "MProxyETC", "0x83D557A1E88C9E3BbAe51DFA7Bd12CF523B28b84", "ETC");
             PoolClient ethereumPool2 = new PoolClient(ethPoolInstance2, poolHandler2, coinHandler2);
-            */
+            
 
             Console.ReadLine();
         }
