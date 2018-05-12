21a830bbbb2c5366a981dffd83ffe0b8ea6b3037
LostSoulFly <lostsoulfly@users.noreply.github.com>
2018-05-12 16:53:48 -0400

    Okay.
    I took a look at how I wanted to store miner stats, and I think I'm going to move forward with a separate class that stores all the miner stats for a particular pool instance.
    
    It will be handled by the MinerManager for that pool. This will allow miners to come and go, but store their stats based on their RigName, possibly for use in a Web UI or other app in the future.

--- MinerProxy2/AssemblyInfo.cs
+++ MinerProxy2/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.53.22")]
-[assembly: AssemblyFileVersion("0.1.53.22")]
+[assembly: AssemblyVersion("0.1.63.1242")]
+[assembly: AssemblyFileVersion("0.1.63.1242")]
---
+++ MinerProxy2/Miners/MinerStats.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinerProxy2.Miners
+{
+    class MinerStats
+    {
+    }
+}
